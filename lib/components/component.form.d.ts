f7c1d9e358671db932de6ff0d74ca89071d3f45f
Kartik Rao <kartik.rao@adinfinity.com>
2019-02-07 21:02:11 +1100

    Remove bad css import

--- lib/components/component.form.d.ts
+++ lib/components/component.form.d.ts
@@ -1,5 +1,4 @@
 import * as React from "react";
 import { IFormProps } from "@adinfinity/ai-core-forms";
-import '../app.css';
 declare const _default: React.ComponentClass<import("antd/lib/form/Form").RcBaseFormProps & Pick<IFormProps, "id" | "name" | "exid" | "desc" | "tenant" | "status" | "content" | "values" | "layout" | "formLayoutOptions">>;
 export default _default;
