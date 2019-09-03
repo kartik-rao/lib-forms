7a5599174d8d392c65af6be4a6dcab8ac756cb95
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-03 15:56:44 +1000

    3.4.0

--- lib/components/canvas/Canvas.d.ts
+++ lib/components/canvas/Canvas.d.ts
@@ -1,2 +1,4 @@
 import * as React from "react";
+import "../../app.css";
+import "@kartikrao/lib-forms-core/src/app.css";
 export declare const Canvas: React.FC<any>;
--- lib/components/canvas/partials/dnd.common.d.ts
+++ lib/components/canvas/partials/dnd.common.d.ts
@@ -1,4 +1,11 @@
-export declare const ItemList: any;
-export declare const Container: any;
+export declare const DraggableItemList: any;
+export declare const DraggableItem: any;
 export declare const getBadgeStyle: (type: string) => "cyan" | "gold" | "green" | "magenta" | "red" | "geekblue";
 export declare const getItemStyle: (isDragging: boolean, draggableStyle: any) => any;
+export declare const getItemListStyle: (isDraggingOver: boolean, type: string) => {
+    userSelect: string;
+    padding: string;
+    marginTop: string;
+    border: string;
+    borderRadius: string;
+};
