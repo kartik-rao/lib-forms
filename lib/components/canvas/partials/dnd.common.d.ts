d257c1a2117b64796f2523049ff8f84428d83333
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-13 17:03:30 +1000

    3.7.3

--- lib/components/canvas/partials/dnd.common.d.ts
+++ lib/components/canvas/partials/dnd.common.d.ts
@@ -1,5 +1,5 @@
-export declare const DraggableItemList: import("styled-components").StyledComponent<"div", any, any, never>;
-export declare const DraggableItem: import("styled-components").StyledComponent<"div", any, any, never>;
+export declare const DraggableItemList: any;
+export declare const DraggableItem: any;
 export declare const getBadgeStyle: (type: string) => "cyan" | "gold" | "green" | "magenta" | "red" | "geekblue";
 export declare const getItemStyle: (isDragging: boolean, draggableStyle: any) => any;
 export declare const getItemListStyle: (isDraggingOver: boolean, type: string) => {
