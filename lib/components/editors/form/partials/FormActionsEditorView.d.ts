8fc41d7ca02652dd910591a244f4d9535573003e
Kartik Rao <kartik.rao@adinfinity.com>
2019-09-17 10:20:18 +1000

    3.7.5

---
+++ lib/components/editors/form/partials/FormActionsEditorView.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+declare const _default: import("antd/lib/form/interface").ConnectedComponentClass<({ form: { getFieldDecorator, validateFieldsAndScroll } }: {
+    form: {
+        getFieldDecorator: any;
+        validateFieldsAndScroll: any;
+    };
+}) => JSX.Element, Pick<import("antd/lib/form/Form").FormComponentProps<any>, "wrappedComponentRef">>;
+export default _default;
